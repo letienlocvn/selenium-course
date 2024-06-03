b45f3385670a71bf6c130604c31d5e9dfb8c9ed8
Le Tien Loc <letienlocvn@gmail.com>
2024-06-03 09:12:14 +0700

    Relation of parent and child when dropdown has multiple value

--- src/main/java/section_one/HandlingUIApplication.java
+++ src/main/java/section_one/HandlingUIApplication.java
@@ -3,12 +3,12 @@ package section_one;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class HandlingUIApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        driver.manage().window().maximize();
 
         WebElement dropdownElement = driver.findElement(By.id("divpaxinfo"));
 
@@ -27,5 +27,15 @@ public class HandlingUIApplication {
         doneButton.click();
 
         System.out.println(dropdownElement.getText());
+
+
+        // Handle for Dynamic dropdown
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//a[@value='MAA']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='BKK']")).click();
+
     }
 }
